43c19e112e4f9251e6e49da7712557b680024389
PatrikMrvic <patrik.mrvic@skole.hr>
2020-02-29 14:00:22 +0100

    Moj prvi init

---
+++ C1tutorial/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C1tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
