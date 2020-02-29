e9e525737a8ba7a3fec5afffc372bdd6859580a1
PatrikMrvic <patrik.mrvic@skole.hr>
2020-02-29 16:12:14 +0100

    Nizovi

---
+++ 2_nizovi/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _2_nizovi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nizovi!");
+            int[] cijelobrojni = new int[10];  //0-9
+            cijelobrojni[0] = 5;
+            cijelobrojni[7] = 9;
+            Console.WriteLine($"ukupno brojeva {cijelobrojni.Length }");
+
+            foreach (var br in cijelobrojni )
+
+            {
+                Console.WriteLine($"broj {br }");
+            }
+
+            int[] niz = new int[] { 1, 2, 4, 66, 78, 89, 45 };
+            foreach (var item in niz)
+            {
+                Console.Write(" " + item*item);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i);
+                cijelobrojni[i] = i;
+            }
+            Console.WriteLine();
+            foreach (var br in cijelobrojni)
+
+            {
+                Console.WriteLine($"broj {br }");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 10; i+=2)
+            {
+                Console.Write(cijelobrojni[i]+" ");
+                
+            }
+            Console.WriteLine();
+            for (int i = 9; i>0; i--)
+            {
+                Console.Write(cijelobrojni[i] + " ");
+
+            }
+            Console.WriteLine();
+            for (int i = 9; i > 0; i-=2)
+            {
+                Console.Write(cijelobrojni[i] + " ");
+
+            }
+        }
+    }
+}
