6d9c26d9da71347be909457091df0ccbee1d820e
PatrikMrvic <patrik.mrvic@skole.hr>
2020-02-29 14:39:07 +0100

    moj rectangle

--- C1tutorial/Program.cs
+++ C1tutorial/Program.cs
@@ -7,6 +7,12 @@ namespace C1tutorial
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Rectangle r = new Rectangle();
+            r.Acceptdetails();
+            r.Display();
+            Rectangle r2 = new Rectangle(2, 5);
+            r2.Display();
+
 
         }
     }
---
+++ C1tutorial/Rectangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C1tutorial
+{
+    class Rectangle
+    {// member variables
+        double length;
+        double width;
+        
+
+        public Rectangle(int v1, int v2)
+        {
+            this.length = v1;
+            this.width = v2;
+        }
+
+        public Rectangle()
+        {
+        }
+
+        public void Acceptdetails()
+        {
+            length = 4.5;
+            width = 3.5;
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+            Console.WriteLine("Pravokutnik {0} {1} ima površinu u {2} ", length, width, length * width);
+            Console.WriteLine("Pravokutnik {0} {1} ima opseg u {2} ", length, width, (length + width)*2);
+
+        }
+    }
+    
+}
